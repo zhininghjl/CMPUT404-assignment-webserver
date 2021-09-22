9b096029b9f2d0a2ef14dbff9b0771a9a361442b
zhininghjl <zhininghjl@gmail.com>
2021-09-21 23:03:32 -0600

    add request start line data extraction + determine mime type

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 #  coding: utf-8 
 import socketserver
+from typing import Protocol
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
 # 
@@ -33,6 +34,36 @@ class MyWebServer(socketserver.BaseRequestHandler):
         self.data = self.request.recv(1024).strip()
         print ("Got a request of: %s\n" % self.data)
         self.request.sendall(bytearray("OK",'utf-8'))
+
+        root = "./www" # root+start_line[1],  # path of the resource e.g. "./www/base.html"
+        method, path, protocol = self.parse_request()  # a dictionary that contains the data of the request's start-line
+        print("start_line_data: ", method, path, protocol)
+        
+        mime_type = self.get_mime_type(path)
+        print("mime_type: %s\n" % mime_type)
+
+    # site: https://stackoverflow.com/questions/18563664/socketserver-python
+    def parse_request(self):
+        # decoded byte object to string object, then split by \r\n
+        # ['GET / HTTP/1.1', 'Host: 127.0.0.1:8080', 'User-Agent: curl/7.64.1', 'Accept: */*']
+        lines = self.data.decode("utf-8").splitlines()
+        start_line = lines[0].split()
+        return start_line[0], start_line[1], start_line[2]
+
+    def get_mime_type(self, path):
+        if "/" == path:
+            return "root"
+
+        if path.endswith("/"):
+            path = path[:-1]  # remove / at the end
+        
+        if "/" in path:
+            p = path.split("/")[-1]
+            if "." in p:
+                if len(p.split(".")) == 2:
+                    return "text/"+p.split(".")[1]
+        return "invalid"
+
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
